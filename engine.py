4116eeee65bcc32c5e1f36d3d57b22c756ab1e34
lor1cd <laurrains@gmail.com>
2023-09-26 18:57:13 +0200

    engine update for "min"

--- engine.py
+++ engine.py
@@ -23,65 +23,66 @@ class Engine:
                 self.log = log
                 self.jsprms = jsprms                                
                 self.root_app = os.getcwd()
+                self.modes = self.jsprms.prms['modes']['musical']
+                self.notes = self.jsprms.prms['notes']
 
         @_error_decorator()
-        def get_mode_idx(self, modes, code_mode):
+        def get_mode_idx(self, code_mode):
                 # print(modes)                
-                for idx_mode, mode in enumerate(modes):
+                for idx_mode, mode in enumerate(self.modes):
                         if mode['code'] == code_mode:
                                 # print(idx_mode)
                                 # print(self.jsprms.prms['modes']['musical'][idx_mode]['mode'])
                                 return idx_mode                        
 
         @_error_decorator()
-        def get_note_idx(self, notes, code_note):
+        def get_note_idx(self, code_note):
                 # idx_note = 0
-                for idx_note, note in enumerate(notes):                
+                for idx_note, note in enumerate(self.notes):                
                         if note['code'] == code_note:
                                 # print(idx_note)
                                 # print(self.jsprms.prms['notes'][idx_note]['code'])
                                 return idx_note
                 
         @_error_decorator()
-        def get_note_input(self, notes):
+        def get_note_input(self):
                 lnotes = ''
-                for n in notes :
+                for n in self.notes :
                         lnotes += f"{n['code']} "                
                 return input(f'note : ({lnotes}) : ')   
 
         @_error_decorator()
-        def get_mode_input(self, modes):
+        def get_mode_input(self):
                 lmodes = ''
-                for m in modes :
+                for m in self.modes :
                         lmodes += f"{m['code']} "                
                 return input(f'mode : ({lmodes}) : ')   
         
 
         @_error_decorator()
-        def get_modes_by(self):
+        def get_modes_by(self, code_note, code_mode):
                 self.trace(inspect.stack()[0])
-                modes = self.jsprms.prms['modes']['musical']
-                notes = self.jsprms.prms['notes']
-                code_note = self.get_note_input(notes) 
-                code_mode = self.get_mode_input(modes)
-                                
-                idx_mode = self.get_mode_idx(modes, code_mode)
-                idx_note = self.get_note_idx(notes, code_note)
+                
+                idx_mode = self.get_mode_idx(code_mode)
+                idx_note = self.get_note_idx(code_note)
                 idx = idx_mode
                 idx_c = idx_note
-                for i in range(0, len(modes)):                                               
-                        print(f"{notes[idx_c]['code']} {modes[idx]['mode']}")
+                for i in range(0, len(self.modes)):           
+                        #print(f'@@@@@@@@@@@@@ idx_c={idx_c} idx={idx}')                                    
+                        print(f"{self.notes[idx_c]['code']} {self.modes[idx]['mode']}")
                         # print(modes[idx]['semiton'])
                         # print(notes[idx_c]['code'])
-                        idx_c += modes[idx]['semiton']
-                        # print(f'{idx_c} = idx_c')
-                        if idx_c == len(notes):
-                                idx_c-= len(notes)
+                        # pdb.set_trace()
+                        idx_c += self.modes[idx]['semiton']
+                        # print(f'{idx_c} = idx_c')                        
+                        if idx_c >= len(self.notes):
+                                idx_c-= len(self.notes)
+                                print(f'idx_c = {idx_c}')
                         
                         print('#######################')
                         # input()
                         idx+=1
-                        if idx == len(modes):
+                        if idx == len(self.modes):
                            idx = 0
 
 
